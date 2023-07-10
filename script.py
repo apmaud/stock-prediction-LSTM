f417defa4ba050e6da3d75e2c0be70653d88ae62
Arjun Maudhoo <apm526@nyu.edu>
2023-07-10 19:47:47 -0400

    Add all libraries

---
+++ script.py
@@ -0,0 +1,13 @@
+from pandas_datareader import data # access financial data from sources
+import matplotlib.pyplot as plt # collection of functions that make change to a figure
+import pandas as pd # general analysis functions
+import datetime as dt # datetime
+import urllib.request, json # opens and reads URLs
+import os # allows running of commands in the script
+import numpy as np # used for array work
+import tensorflow as tf # machine learning
+from sklearn.preprocessing import MinMaxScaler # machine learning, data analysis library
+
+
+
+
