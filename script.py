764167fe713153dc4106e683be049836d14198f5
Arjun Maudhoo <apm526@nyu.edu>
2023-07-11 15:17:27 -0400

    Import of data/info

--- script.py
+++ script.py
@@ -8,6 +8,15 @@ import numpy as np # used for array work
 import tensorflow as tf # machine learning
 from sklearn.preprocessing import MinMaxScaler # machine learning, data analysis library
 
+data_source = 'kaggle'
+
+if data_source == 'alphavantage':
+    api_key = '57RJJOC80BF61120'
+    ticker = "AAL"
+    url_string = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=%s&outputsize=full&apikey=%s"%(ticker,api_key)
+    file_to_save = 'stock_market_data-%s.csv'%ticker
+    
+
 
 
 
